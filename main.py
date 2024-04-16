79b936d23da76cf4e8b7815f5c4cc67e1a01dbe2
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-04-16 13:00:06 +0200

    New Project

---
+++ main.py
@@ -0,0 +1,47 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if not self.root:
+            self.root = TreeNode(value)
+        else:
+            self._insert_recursively(self.root, value)
+
+    def _insert_recursively(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._insert_recursively(node.left, value)
+        elif value > node.value:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._insert_recursively(node.right, value)
+
+    def inorder_traversal(self, node):
+        if node:
+            self.inorder_traversal(node.left)
+            print(node.value, end=' ')
+            self.inorder_traversal(node.right)
+
+
+if __name__ == "__main__":
+    tree = BinaryTree()
+    tree.insert(10)
+    tree.insert(5)
+    tree.insert(15)
+    tree.insert(3)
+    tree.insert(8)
+    tree.insert(12)
+    tree.insert(20)
+
+    print("Result inorder_traversal:")
+    tree.inorder_traversal(tree.root)
